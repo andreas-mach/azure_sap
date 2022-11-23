aeb6b416720652ea17fe620047b61cb8d24c3266
andreas-mach <amach@suse.com>
2022-11-23 10:54:36 +0100

    modified configure_deployer.sh

--- sap-automation/deploy/scripts/configure_deployer.sh
+++ sap-automation/deploy/scripts/configure_deployer.sh
@@ -221,7 +221,8 @@ ansible_pip3=${ansible_venv_bin}/pip3
 # Azure SAP Automated Deployment directories
 asad_home="${HOME}/Azure_SAP_Automated_Deployment"
 asad_ws="${asad_home}/WORKSPACES"
-asad_repo="https://github.com/Azure/sap-automation.git"
+#asad_repo="https://github.com/Azure/sap-automation.git"
+asad_repo="https://github.com/andreas-mach/azure_sap.git"
 asad_dir="${asad_home}/$(basename ${asad_repo} .git)"
 
 # Terraform installation directories
